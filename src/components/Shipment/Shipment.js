4fd3d1f8ae351d28e70aa719848ca7a862a85403
Alam Hosen <alamhosen.web@gmail.com>
2022-04-11 21:03:32 +0600

    updated shipping form

---
+++ src/components/Shipment/Shipment.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+
+const Shipment = () => {
+    const [user] = useAuthState(auth);
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [address, setAddress] = useState('');
+    const [phone, setPhone] = useState('');
+    const [error, setError] = useState('');
+
+    const handleNameBlur = event =>{
+        setName(event.target.value);
+    }
+
+    const handleAddressBlur = event =>{
+        setAddress (event.target.value);
+    }
+
+    const handlePhoneBlur = event =>{
+        setPhone (event.target.value);
+    }
+
+    const handleCrateUser = event => {
+        event.preventDefault();
+      
+    }
+
+    return (
+        <div className='form-container'>
+            <div>
+                <h2 className='form-title'>Shipping Information</h2>
+                <form onSubmit={handleCrateUser}>
+                    <div className="input-group">
+                        <label htmlFor="name">Your Name</label>
+                        <input onBlur={handleNameBlur} type="text" name="name" id="name" required />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="email">Your Email</label>
+                        <input value={user?.email} readOnly type="email" name="email" id="email" required />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="address">Address</label>
+                        <input onBlur={handleAddressBlur} type="text" name="address" id="address" required />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="phone">Phone Number</label>
+                        <input onBlur={handlePhoneBlur} type="text" name="phone" id="phone" required />
+                    </div>
+                    <p style={{color: 'red'}}>{error}</p>
+                    <input className='form-submit' type="submit" value="Add Shipping" />
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Shipment;
